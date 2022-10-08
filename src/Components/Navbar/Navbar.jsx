5d63e16b41a362ba44fa0de798160f2537f10d73
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-10-08 08:54:52 -0400

    Added more components

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import navbarStyle from './Navbar.module.css'
+import logo from '../../Assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <img src={logo} alt="logo"/>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

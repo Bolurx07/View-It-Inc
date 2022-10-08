cb191cb29534e87b7cdec7a38be6d3d33dc7d397
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-10-08 11:22:46 -0400

    Done with the Navbar component.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 import navbarStyle from './Navbar.module.css'
 import logo from '../../Assets/logo.svg'
+import notificationicon from '../../Assets/notificon.svg'
+import recordicon from '../../Assets/recordicon.svg'
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
+    <div className={navbarStyle.navbar}>
+        <div className={navbarStyle.left}>
             <img src={logo} alt="logo"/>
-            <h1>View It Inc</h1>
+            <h1 className={navbarStyle.header}>View It Inc</h1>
+        </div>
+        <div className={navbarStyle.right}>
+            <img src={notificationicon} alt="notification" />
+            <img src={recordicon} alt="recordicon" />
         </div>
     </div>
   )
